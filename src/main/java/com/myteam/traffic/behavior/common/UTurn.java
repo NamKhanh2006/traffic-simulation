7d57c929af23794105aa388f7c3fbc6ba5c92f2c
えんぴし１４８２ <quangdm06@gmail.com>
2026-04-30 22:00:43 +0700

    Update UTurn.java

--- src/main/java/com/myteam/traffic/behavior/common/UTurn.java
+++ src/main/java/com/myteam/traffic/behavior/common/UTurn.java
@@ -1,9 +1,9 @@
 package com.myteam.traffic.behavior.common;
 
-//import simulation.vehicle.Vehicle;
+import simulation.vehicle.Vehicle;
 
 public class UTurn {
-    /*public static void uTurn(Vehicle v) {
+    public static void uTurn(Vehicle v) {
         System.out.println(v.getType() + " quay dau");
-    }*/
+    }
 }
