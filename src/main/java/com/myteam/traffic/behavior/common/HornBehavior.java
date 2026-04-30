c5d4055f7a313c6f4fc81fb0080d736d34fb1a48
えんぴし１４８２ <quangdm06@gmail.com>
2026-04-30 21:38:39 +0700

    Update HornBehavior.java

--- src/main/java/com/myteam/traffic/behavior/common/HornBehavior.java
+++ src/main/java/com/myteam/traffic/behavior/common/HornBehavior.java
@@ -1,9 +1,9 @@
 package com.myteam.traffic.behavior.common;
 
-//import Traffic.vehicle.Vehicle;
+import Traffic.vehicle.Vehicle;
 
 public class HornBehavior {
-    /*public static void honk(Vehicle v) {
+    public static void honk(Vehicle v) {
         System.out.println(v.getType() + " bam coi!");
-    }*/
+    }
 }
