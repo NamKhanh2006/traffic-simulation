c9595cde50c72a3f89da190ff6e38b3143133636
えんぴし１４８２ <quangdm06@gmail.com>
2026-04-30 22:01:26 +0700

    Update DriverBehavior.java

--- src/main/java/com/myteam/traffic/behavior/DriverBehavior.java
+++ src/main/java/com/myteam/traffic/behavior/DriverBehavior.java
@@ -1,8 +1,8 @@
 package com.myteam.traffic.behavior;
 
-//import Traffic.vehicle.Vehicle;
-//import Traffic.traffic.TrafficLight;
+import Traffic.vehicle.Vehicle;
+import Traffic.traffic.TrafficLight;
 
 public class DriverBehavior {
-    //void decide(Vehicle self, Vehicle front, TrafficLight light);
+    void decide(Vehicle self, Vehicle front, TrafficLight light);
 }
