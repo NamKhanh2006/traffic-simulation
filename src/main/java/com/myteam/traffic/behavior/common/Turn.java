a8f21afade4d87cfe7dcb6cf06357d91ce56ba2d
えんぴし１４８２ <quangdm06@gmail.com>
2026-05-03 17:58:57 +0700

    Update Turn.java

--- src/main/java/com/myteam/traffic/behavior/common/Turn.java
+++ src/main/java/com/myteam/traffic/behavior/common/Turn.java
@@ -1,9 +1,14 @@
 package com.myteam.traffic.behavior.common;
 
-import Traffic.vehicle.Vehicle;
+import traffic.common.Action;
+import traffic.model.context.RoadContext;
+import traffic.model.vehicle.Vehicle;
 
 public class Turn {
-    public static void turn(Vehicle v) {
-        System.out.println(v.getType() + " re");
+    public static Action decide(Vehicle v, RoadContext c) {
+        if (c.isAtIntersection() && c.hasTurnSignal()) {
+            return Action.TURN;
+        }
+        return null;
     }
 }
