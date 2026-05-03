c83a10b023dacc57ca80a92b4e59fdb4e4335f91
えんぴし１４８２ <quangdm06@gmail.com>
2026-05-03 16:29:38 +0700

    Update NormalDriver.java

--- src/main/java/com/myteam/traffic/behavior/NormalDriver.java
+++ src/main/java/com/myteam/traffic/behavior/NormalDriver.java
@@ -1,28 +1,23 @@
 package com.myteam.traffic.behavior;
 
-import Traffic.vehicle.Vehicle;
-import Traffic.traffic.TrafficLight;
-import Traffic.behavior.common.*;
+import traffic.common.Action;
+import traffic.model.context.RoadContext;
+import traffic.model.vehicle.Vehicle;
 
-public class NormalDriver extends DriverBehavior {
+public class NormalDriver implements DriverBehavior {
 
     @Override
-    public void decide(Vehicle self, Vehicle front, TrafficLight light) {
+    public Action decideAction(Vehicle v, RoadContext c) {
 
-        DistanceKeeping.keepDistance(self, front);
-
-        if (light.isRed()) {
-            self.stop();
-            System.out.println("Normal: dung den do");
-            return;
+        if (c.getFrontVehicle() != null) {
+            double dist = c.getFrontVehicle().getX() - v.getX();
+            if (dist < 5) return Action.SLOW_DOWN;
         }
 
-        if (front != null && front.isEmergency()) {
-            self.slowDown();
-            LaneChange.changeLane(self);
-            System.out.println("Normal: nhuong duong xe uu tien");
-        }
+        if (c.hasRedLightAhead()) return Action.STOP;
 
-        self.moveForward();
+        if (c.hasEmergencyNearby()) return Action.CHANGE_LANE;
+
+        return Action.MOVE_FORWARD;
     }
 }
