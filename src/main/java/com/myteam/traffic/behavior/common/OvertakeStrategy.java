c992e4f3203769b5d45388fe28f982d46dc75da5
えんぴし１４８２ <quangdm06@gmail.com>
2026-05-03 17:14:35 +0700

    Update OvertakeStrategy.java

--- src/main/java/com/myteam/traffic/behavior/common/OvertakeStrategy.java
+++ src/main/java/com/myteam/traffic/behavior/common/OvertakeStrategy.java
@@ -1,10 +1,20 @@
 package com.myteam.traffic.behavior.common;
 
-import Traffic.vehicle.Vehicle;
+import traffic.common.Action;
+import traffic.model.context.RoadContext;
+import traffic.model.vehicle.Vehicle;
 
-public class OvertakeStrategy {
-    public static void overtake(Vehicle v) {
-        System.out.println(v.getType() + " vuot xe");
-        v.accelerate();
+public class OvertakeDecision {
+
+    public static Action decide(Vehicle v, RoadContext c) {
+        if (c.getFrontVehicle() == null) return null;
+
+        double dist = c.getFrontVehicle().getX() - v.getX();
+
+        if (dist < 8 && c.canChangeLane()) {
+            return Action.OVERTAKE;
+        }
+
+        return null;
     }
 }
