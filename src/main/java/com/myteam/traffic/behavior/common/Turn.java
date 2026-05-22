6d48e7f4020a8250ec662983ae75ef9472b3a0ef
えんぴし１４８２ <quangdm06@gmail.com>
2026-05-22 22:51:23 +0700

    Update Turn.java

--- src/main/java/com/myteam/traffic/behavior/common/Turn.java
+++ src/main/java/com/myteam/traffic/behavior/common/Turn.java
@@ -1,14 +1,31 @@
-package com.myteam.traffic.behavior.common;
+package behavior.common;
 
-import traffic.common.Action;
-import traffic.model.context.RoadContext;
 import traffic.model.vehicle.Vehicle;
 
+/**
+ * Utility class for turning manoeuvres (left or right turn).
+ *
+ * Used at intersections when a driver behaviour decides the vehicle
+ * must turn. The direction is expressed as an enum to make call sites
+ * readable and to allow the GUI team to animate the correct arc.
+ */
 public class Turn {
-    public static Action decide(Vehicle v, RoadContext c) {
-        if (c.isAtIntersection() && c.hasTurnSignal()) {
-            return Action.TURN;
-        }
-        return null;
+
+    /** The direction in which to turn. */
+    public enum Direction {
+        LEFT, RIGHT
+    }
+
+    private Turn() { /* utility class – no instances */ }
+
+    /**
+     * Execute a turn in the given direction for the specified vehicle.
+     *
+     * @param v         the vehicle turning
+     * @param direction LEFT or RIGHT
+     */
+    public static void turn(Vehicle v, Direction direction) {
+        System.out.println(v.getType() + " turning " + direction.name().toLowerCase());
+        v.turn(direction);
     }
 }
