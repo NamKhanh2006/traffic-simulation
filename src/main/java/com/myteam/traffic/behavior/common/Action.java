eba07629d5738cf0470da93d3b3317ce6b4fbdf2
えんぴし１４８２ <quangdm06@gmail.com>
2026-05-22 22:53:13 +0700

    Add files via upload

---
+++ src/main/java/com/myteam/traffic/behavior/common/Action.java
@@ -0,0 +1,14 @@
+package Behavior.common;
+
+public enum Action {
+    MOVE_FORWARD,
+    STOP,
+    SLOW_DOWN,
+    ACCELERATE,
+    CHANGE_LANE,
+    OVERTAKE,
+    HONK,
+    TURN_LEFT,
+    TURN_RIGHT,
+    U_TURN
+}
